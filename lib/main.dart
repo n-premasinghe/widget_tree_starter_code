2af6a8074ce4d424ae82cbb97a346befedd03e0e
n-premasinghe <99045891+n-premasinghe@users.noreply.github.com>
2022-05-09 08:16:00 -0400

    Code for reference during the live dev process

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,31 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget> [
+              CircleAvatar(
+                radius: 50.0,
+                //backgroundImage:,
+              ),
+              Text(
+                'Noah Premasinghe',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                'Some guy idk',
+                style: TextStyle(
+                  
+                ), 
+              )
+            ],
+          )
+          ),
       ),
     ),
   );
