fb7125744aa607ce7ee3880301a5b506c0dacc35
n-premasinghe <99045891+n-premasinghe@users.noreply.github.com>
2022-05-10 22:05:29 -0400

    Step-1 1st rev.

--- lib/main.dart
+++ lib/main.dart
@@ -4,77 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
-              CircleAvatar(
-                radius: 70,
-                //backgroundImage:,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 4),
-                child: Text(
-                  'Noah Premasinghe',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Some guy idk',
-                  style: TextStyle(
-                    color: Colors.teal.shade100,
-                    fontSize: 20,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ), 
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      '(705)-420-6969',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontSize: 20,
-
-                      ),
-                    )
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: ListTile(
-                    leading: Icon(
-                      Icons.mail,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      '676930@smcdsb.on.ca',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontSize: 20
-                      ),
-                    )
-                ),
-              )
-            ],
-          )
-          ),
+        
       ),
     ),
   );
